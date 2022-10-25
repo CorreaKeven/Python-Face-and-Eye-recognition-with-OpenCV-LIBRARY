6f8fcf374868386fc6befa88f69b21446eae8fc8
CorreaKeven <103259588+CorreaKeven@users.noreply.github.com>
2022-10-25 20:42:37 -0300

    Add files via upload
    25/10/22

--- 251022-aula1python.py
+++ 251022-aula1python.py
@@ -9,3 +9,6 @@ print(vetor_imagem_pb)
 classificador=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 faces = classificador.detectMultiScale(vetor_imagem_pb)
 print(faces)
+for (x, y, w, h) in faces:
+    cv2.rectangle(vetor_imagem, (x, y),(x+w,y+h), (255, 0, 0), 5)
+cv2.imshow("foto detectada",vetor_imagem)
